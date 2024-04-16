4a642b025bd11cbe64adf0af92f262be9893ede7
moonwitch <joshua_develops@proton.me>
2024-04-16 19:03:39 +0200

    feat: added Billboard to exported components

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,3 @@
 export { default as BasicButton } from './components/buttons/BasicButton.vue';
 export { default as BasicInput } from './components/inputs/BasicInput.vue';
+export { default as Billboard } from './components/typography/Billboard.vue';
